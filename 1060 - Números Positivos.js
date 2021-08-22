f4b68f64109fb24e2a9959fbc154b0a7921dcea2
Marco Jardim <70395937+Marco-Jardim872@users.noreply.github.com>
2021-08-22 19:29:10 -0300

    Add files via upload

---
+++ 1060 - Números Positivos.js
@@ -0,0 +1,20 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var dados = input.split('\n');
+
+let val1 = parseFloat(dados.shift());
+let val2 = parseFloat(dados.shift());
+let val3 = parseFloat(dados.shift());
+let val4 = parseFloat(dados.shift());
+let val5 = parseFloat(dados.shift());
+let val6 = parseFloat(dados.shift());
+
+let arr=[];
+let pos=0;
+arr.push(val1,val2,val3,val4,val5,val6);
+
+for (let i = 0; i < arr.length; i++){
+    if(arr[i]>0){
+        pos++;
+    }
+}
+console.log(pos + " valores positivos");
